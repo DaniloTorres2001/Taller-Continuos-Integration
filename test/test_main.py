057fe38a2115f6753d3dc1efd24dc2ae5bc8cff4
DaniloTorres2001 <dantvera@espol.edu.ec>
2023-07-21 00:07:36 -0500

    Fix for tests

--- test/test_main.py
+++ test/test_main.py
@@ -1,20 +1,18 @@
 import pytest
 from main import (
+    display_menu,
+    get_order,
     calculate_total_cost,
+    apply_discount,
+    apply_special_meal_surcharge,
+    apply_special_offer_discount,
     validate_meal_availability,
 )
 
-@pytest.fixture
-def sample_order():
-    return {
-        1: {'quantity': 2, 'price': 8.50},
-        3: {'quantity': 3, 'price': 6.75},
-        5: {'quantity': 1, 'price': 10.25},
-    }
 
-
+# Define the example menu as a fixture to reuse it in the tests
 @pytest.fixture
-def sample_menu():
+def example_menu():
     return [
         ("Fried Rice", 8.50, "Chinese Food"),
         ("Spaghetti", 12.99, "Italian Food"),
@@ -25,13 +23,93 @@ def sample_menu():
     ]
 
 
-def test_calculate_total_cost(sample_order):
-    total_cost = calculate_total_cost(sample_order)
-    assert total_cost == pytest.approx(8.50 * 2 + 6.75 * 3 + 10.25, abs=1e-2)
+# Test display_menu function
+def test_display_menu(capsys, example_menu):
+    display_menu(example_menu)
+    captured = capsys.readouterr()
+    assert "Menu:" in captured.out
+    assert "1. Fried Rice - Chinese Food ($8.50)" in captured.out
 
 
-def test_validate_meal_availability(sample_order, sample_menu):
-    assert validate_meal_availability(sample_order.keys(), sample_menu) is True
-    assert validate_meal_availability([0, 2, 4], sample_menu) is False  # Meal 0 and 4 are unavailable
+# Test get_order function
 
-# Additional test cases can be added to further verify the functions
+
+# Test calculate_total_cost function
+def test_calculate_total_cost(example_menu):
+    order = {1: {'quantity': 2, 'price': 8.50}, 3: {'quantity': 1, 'price': 6.75}}
+    total_cost = calculate_total_cost(order)
+    assert total_cost == 8.50 * 2 + 6.75
+
+
+# Test apply_discount function
+def test_apply_discount_greater_than_10(example_menu):
+    total_cost = 100.00
+    total_quantity = 12
+    discounted_cost = apply_discount(total_quantity, total_cost)
+    assert discounted_cost == 80.00
+
+
+def test_apply_discount_greater_than_5(example_menu):
+    total_cost = 70.00
+    total_quantity = 6
+    discounted_cost = apply_discount(total_quantity, total_cost)
+    assert discounted_cost == 63.00
+
+
+def test_apply_discount_less_than_5(example_menu):
+    total_cost = 40.00
+    total_quantity = 4
+    discounted_cost = apply_discount(total_quantity, total_cost)
+    assert discounted_cost == 40.00
+
+
+# Test apply_special_meal_surcharge function
+def test_apply_special_meal_surcharge(example_menu):
+    order = {1: {'quantity': 2, 'price': 8.50}, 6: {'quantity': 3, 'price': 18.75}}
+    surcharge = apply_special_meal_surcharge(order, example_menu)
+    assert surcharge == 18.75 * 3 * 0.05
+
+
+def test_apply_special_meal_surcharge_no_special_meals(example_menu):
+    order = {1: {'quantity': 2, 'price': 8.50}, 3: {'quantity': 1, 'price': 6.75}}
+    surcharge = apply_special_meal_surcharge(order, example_menu)
+    assert surcharge == 0
+
+
+# Test apply_special_offer_discount function
+def test_apply_special_offer_discount_greater_than_100(example_menu):
+    total_cost = 120.00
+    discounted_cost = apply_special_offer_discount(total_cost)
+    assert discounted_cost == 120.00 - 25
+
+
+def test_apply_special_offer_discount_greater_than_50(example_menu):
+    total_cost = 60.00
+    discounted_cost = apply_special_offer_discount(total_cost)
+    assert discounted_cost == 60.00 - 10
+
+
+def test_apply_special_offer_discount_less_than_50(example_menu):
+    total_cost = 40.00
+    discounted_cost = apply_special_offer_discount(total_cost)
+    assert discounted_cost == 40.00
+
+
+# Test validate_meal_availability function
+def test_validate_meal_availability_valid(example_menu):
+    order = {1: {'quantity': 2, 'price': 8.50}, 3: {'quantity': 1, 'price': 6.75}}
+    valid = validate_meal_availability(order.keys(), example_menu)
+    assert valid is True
+
+
+def test_validate_meal_availability_invalid(example_menu, capsys):
+    order = {1: {'quantity': 2, 'price': 8.50}, 3: {'quantity': 1, 'price': 6.75}, 7: {'quantity': 1, 'price': 15.50}}
+    valid = validate_meal_availability(order.keys(), example_menu)
+    captured = capsys.readouterr()
+    assert valid is False
+    assert "Invalid selection: The following meal(s) are unavailable." in captured.out
+    assert "- Meal 7" in captured.out
+
+
+
+
